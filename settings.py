15dbfe7690d85996ce1fe02e6ac470a03128a232
Alexandra-Konvisar <46960334+Alexandra-Konvisar@users.noreply.github.com>
2022-06-02 12:38:03 +0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,29 @@
+valid_email = "al66@pf.com"
+valid_password = "1qasw2"
+class UserData:
+    valid_email = "al66@pf.com"
+    valid_password = "1qasw2"
+    invalid_email = "al666@pf.com"
+    invalid_password = "1qasw22"
+    no_register_email = 'alone.test2@mail.ru'
+    no_register_password = '12345'
+
+    invalid_auth_key = {'key':'ss11as'}
+
+class PetsData:
+    name_latin = 'VOLOSATIY'
+    animal_type_latin = 'sapog'
+    name_cyrillic = 'ВОЛОСАТЫЙ'
+    animal_type_cyrillic = 'сапог'
+    name_symbol = '!@#%^&&*()<>?'
+    animal_type_symbol = '?><+_-)(*&^%$#@!'
+    name_numbers = '12345'
+    animal_type_numbers = '098765'
+    name_huge = 'asd'* 1000000
+    age_norm = '2'
+    animal_type_huge = 'qwer'* 1000000
+    age_leter = 'zxcvbФЯЧ'
+    age_symbol = '##$!%@^'
+    age_big  = '12345'* 1000
+    age_huge = '9'* 1000000
+    age_empty = ''
